2cbb0abc59acc9fec37b1ffc6a8037df24f57278
Wayne <wayne.5540@gmail.com>
2018-05-21 15:59:00 +0800

    Build website with very limited data
    * Add basic template by bootstrap 4
    * Define data structure
    * Use vue to render data

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <div id="app">
-    <img src="./assets/rails-phoniex-logo.png">
-    <Landing msg="Welcome to Rails to Phoniex"/>
+  <div class="container" id="app">
+    <div class="row justify-content-center">
+      <img src="./assets/rails-phoniex-logo.png">
+    </div>
+    <div class="row justify-content-center">
+      <Landing :mapping="mapping" />
+    </div>
   </div>
 </template>
 
 <script>
 import Landing from './components/Landing.vue'
+import mapping from './data/mapping.json'
 
 export default {
   name: 'app',
   components: {
     Landing
-  }
+  },
+  data: () => ({
+    mapping: mapping
+  })
 }
 </script>
 
@@ -21,8 +29,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-    </p>
+    <div class="text-center mb-5">
+      <h1>Welcome to Rails to Phoniex</h1>
+      <h3 class="text-muted">Find exilir plugs by ruby gem's name</h3>
+    </div>
+
+    <div class="card mb-3" v-for="card in cards" v-bind:key="card.name">
+      <div class="card-body">
+        <h5 class="card-title">
+          <a :href="card.url">{{ card.name }}</a>
+        </h5>
+        <p class="card-text text-muted" >{{ card.description }}</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item" v-for="plug in card.elixir_plugs" v-bind:key="plug.name">
+          <span>{{ plug.name }}</span> - <a :href="plug.url">{{ plug.url }}</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -12,7 +25,12 @@
 export default {
   name: 'Landing',
   props: {
-    msg: String
+    mapping: Object
+  },
+  computed: {
+    cards: function () {
+      return Object.values(this.mapping)
+    }
   }
 }
 </script>
