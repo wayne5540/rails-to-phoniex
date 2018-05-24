9b1ba13fc2c6f3591cd982816fd5e33c58d8bdf6
Wayne <wayne.5540@gmail.com>
2018-05-24 14:45:54 +0800

    add basic search functionality

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -2,10 +2,19 @@
   <div class="hello">
     <div class="text-center mb-5">
       <h1>Welcome to Rails to Phoniex</h1>
-      <h3 class="text-muted">Find exilir plugs by ruby gem's name</h3>
+      <h3 class="text-muted">Find exilir libraries by ruby gem's name</h3>
     </div>
 
-    <div class="card mb-3" v-for="card in cards" v-bind:key="card.name">
+    <div class="mb-5">
+      <div class="input-group input-group-lg">
+        <input class="form-control" placeholder="Keyword searching..." v-model="searchKey" autofocus>
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary input-group-text" type="button">Search</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card mb-3" v-for="card in cards()" v-bind:key="card.name">
       <div class="card-body">
         <h5 class="card-title">
           <a :href="card.url">{{ card.name }}</a>
@@ -27,9 +36,18 @@ export default {
   props: {
     mapping: Object
   },
-  computed: {
+  data: function () {
+    return {
+      searchKey: ""
+    }
+  },
+  methods: {
     cards: function () {
-      return Object.values(this.mapping)
+      if (this.searchKey == "") {
+        return Object.values(this.mapping)
+      } else {
+        return Object.values(this.mapping).filter((gem) => (gem.name.toLowerCase().includes(this.searchKey.toLowerCase())))
+      }
     }
   }
 }
